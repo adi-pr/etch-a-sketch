e5ab7a6cbc52b423f066fa845180182836ea0c2b
Ruben <adityaprasad7123@gmail.com>
2022-12-06 23:11:34 -0400

    Divs in the grid can now change colours

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const DEFAULT_COLOR = '#333333'
-const DEFAULT_MODE = 'color'
+const DEFAULT_COLOUR = '#333333'
+const DEFAULT_MODE = 'colour'
 const DEFAULT_SIZE = 16
 
-let currentColor = DEFAULT_COLOR
+let currentColour = DEFAULT_COLOUR
 let currentMode = DEFAULT_MODE
 let currentSize = DEFAULT_SIZE
 
-function setCurrentColor(newColor) {
-    currentColor = newColor
+function setCurrentColour(newColour) {
+    currentColour = newColour
 }
 
 function setCurrentMode(newMode) {
@@ -20,6 +20,7 @@ function setCurrentSize(newSize) {
 }
 
 const eraserBtn = document.getElementById('erase-btn')
+const colourBtn = document.getElementById('colour')
 const clearBtn = document.getElementById('clear-btn')
 const sizeBtn = document.getElementById('grid-size-btn')
 const sizeBtn16 = document.getElementById('grid-size-btn16')
@@ -28,6 +29,7 @@ const sizeBtn30 = document.getElementById('grid-size-btn30')
 const grid = document.getElementById('grid')
 
 eraserBtn.onclick = () => setCurrentMode('eraser')
+colourBtn.onclick = () => setCurrentMode('colour')
 sizeBtn.onclick = () => setupGrid(10)
 sizeBtn16.onclick = () => setupGrid(DEFAULT_SIZE)
 sizeBtn25.onclick = () => setupGrid(25)
@@ -64,17 +66,32 @@ function setupGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const gridElement = document.createElement('div')
         gridElement.classList.add('grid-element')
+        gridElement.addEventListener('mouseover', changeColour)
+        gridElement.addEventListener('mousedown', changeColour)
         grid.appendChild(gridElement)
+    }
+}
+
+function changeColour(e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (currentMode === 'colour') {
+        e.target.style.backgroundColor = '#000000'
+    } else if (currentMode === "eraser") {
+        e.target.style.backgroundColor = '#fefefe'
     }
 }
 
 function activateButton(newMode) {
     if (currentMode === 'eraser') {
         eraserBtn.classList.remove('active')
+    } else if (currentMode === 'colour') {
+        colourBtn.classList.remove('active')
     }
 
     if (newMode === 'eraser') {
         eraserBtn.classList.add('active')
+    } else if (newMode === 'colour') {
+        colourBtn.classList.add('active')
     }
 }
 
